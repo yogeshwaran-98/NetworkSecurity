0f42a772493c945c74e0628557cb10840b6659f2
yogeshwaran-98 <151889778+yogeshwaran-98@users.noreply.github.com>
2025-11-27 11:33:03 +0530

    Clear MongoDB URI for security reasons
    Removed sensitive information from MongoDB URI.

--- test_mongodb.py
+++ test_mongodb.py
@@ -3,7 +3,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import urllib.parse
 
-uri = "mongodb+srv://mryogeshwaran98:admin123@cluster0.dfuot.mongodb.net/?appName=Cluster0"
+uri = ""
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
